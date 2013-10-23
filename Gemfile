bb54b941cd9b155bd212af842c0d2fdb7cc10270
chrbradley <chrbradley@gmail.com>
2013-10-23 09:41:17 -0400

    Search twitter directly

--- Gemfile
+++ Gemfile
@@ -3,7 +3,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
-gem 'high_voltage'
+gem 'high_voltage' 
+gem 'twitter' , '4.8.1'
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "Q434bE9AiXFNiZ5bX7Nw"
+  config.consumer_secret = "MtML18R7rdcTIXGJxsRxgPW3xSJ2l5HdhlJgeaRo"
+  config.oauth_token = "1535370710-Sx9zc1LgjRRI4vxFVLQrJFA3uGiPim725dbZqcg"
+  config.oauth_token_secret = "DR5fzmR5I0OpTDIOHk9kxJqbylbgfgw5cjEQup70"
+end
